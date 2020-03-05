b2fe4723e3caddd829cfe698f9551eeb1808bf83
Lukasz Drozdz <lukaszdrozdz@iMac-Lukasz.local>
2020-03-05 10:48:21 +0100

    animation for musings landing page

--- src/scripts/layout/modules/animations.js
+++ src/scripts/layout/modules/animations.js
@@ -1,6 +1,17 @@
 import $ from 'jquery';
 import appear from "jquery.appear"
 
+$.fn.isInViewport = function(offset) {
+    var elementTop = $(this).offset().top;
+    var elementBottom = elementTop + $(this).outerHeight();
+
+    var viewportTop = $(window).scrollTop();
+    var viewportBottom = viewportTop + $(window).height();
+
+//    return elementBottom > viewportTop && elementTop < viewportBottom;
+    return ( elementBottom > viewportTop - offset ) && ( elementTop < viewportBottom + offset );
+};
+
 const Animations = {
     init: function() {
         var dy = -$(window).height()/6;
@@ -29,7 +40,59 @@ const Animations = {
                 }, { accY: dy });
             }
         });
-    }
+    },
+	inViewport: function(){
+		$(window).on('scroll resize', function() {
+		   var offset = -$(window).height()/5;
+		   $('[data-appear]').each( function(i){
+		   		if(typeof attr !== typeof undefined && attr !== false){
+			   		offset = $(this).data('offset');
+		   		}
+			    if ($(this).isInViewport(offset)) {
+				    console.log('inVIEWPORT');
+					if($(this).data('animate') == "allin"){
+						var elem = $(this).find('[data-anim]');
+					}
+					$(elem).each(function(){
+						var type=$(this).data('anim');
+						console.log(type);
+						var typeout = type.replace('In', 'Out');
+						if(typeof $(this).data('delay') != 'undefined'){
+							var delay = $(this).data('delay');
+						}else{
+							var delay = 0;
+						}
+						var el = $(this);
+						setTimeout(function(){
+							el.removeClass(typeout);
+							el.addClass('animated visible '+type);
+						}, delay);
+					});
+			    } else {
+/* ANIMATION OF DISSAPEARING */
+					if($(this).data('animate') == "allin"){
+						var elem = $(this).find('[data-anim]');
+					}
+					$(elem).each(function(){
+						var type=$(this).data('anim');
+						var typeout = type.replace('In', 'Out');
+						if(typeof $(this).data('delay') != 'undefined'){
+							var delay = $(this).data('delay');
+						}else{
+							var delay = 0;
+						}
+						var el = $(this);
+						if($(this).get(0).hasAttribute('data-reversonlythis')){
+							setTimeout(function(){
+								$(el).removeClass(type);
+								$(el).addClass(typeout);
+							}, delay);
+						}
+					});
+			    }
+		    }); 
+		});
+	}
 }
 
 export default Animations
